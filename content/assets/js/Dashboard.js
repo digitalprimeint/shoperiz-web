737303b2f0a1cec902b9b217e53c7e1d281ee2ed
abdielantonioxd <abdielantonio.af@gmail.com>
2019-07-06 23:47:51 -0500

    shoperiz v0.01

--- content/assets/js/Dashboard.js
+++ content/assets/js/Dashboard.js
@@ -1,13 +1,45 @@
 // List of Product 
-$("#list-product").hide();
-$("#list-P").on('click', function () {  
-    $('#list-product').toggle()   
+function initialHide() {
+  $("#list-product").hide();
+  $('#create-product').hide();
+  $('#report-products').hide();
+}
+
+function contentshow() {
+  $('#content').show();
+}
+// list product 
+$("#list-P").on('click', function () {
+  $('#list-product').toggle();
+  $('#content').hide();
 });
 
 $("#list-product-close").on('click', function () {
-  $("#list-product").hide(); 
+  initialHide();
+  contentshow();
 });
 
-$("#viewStatus").on('click',function(){
-$("#status").modal('show');
-})
+// create Product 
+$("#create-P").on('click', function () {
+  $('#create-product').toggle();
+  $('#content').hide();
+});
+
+$("#create-product-close").on('click', function () {
+  initialHide();
+  contentshow();
+});
+
+function selectTemplate(op) {
+  switch (op) {
+    case 'check-template-1':
+      $('#icon-2').removeClass("text-success").addClass( 'text-dark' );
+      $('#icon-1').removeClass('text-dark').addClass( "text-success" );
+      break;
+    case 'check-template-2':
+      $('#icon-1').removeClass('text-success').addClass( "text-dark" );
+      $('#icon-2').removeClass('text-dark').addClass( "text-success" );
+      break;
+  }
+}
+initialHide()
